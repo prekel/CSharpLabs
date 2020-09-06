c0e60ebb3a405ec703dc2ed996dffa3848593ff5
Vladislav Prekel <misterptits@yandex.ru>
2020-09-06 22:08:48 +0700

    projects, sln

---
+++ Lab01/CSharpLabs.Lab01.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpLabs.Lab01.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lab01/CSharpLabs.Lab01.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CSharpLabs.Lab01.Core
+{
+    public class Class1
+    {
+    }
+}
