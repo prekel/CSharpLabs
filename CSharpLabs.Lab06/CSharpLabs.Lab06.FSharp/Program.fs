7f8ff73f0fa0cca30fba93c51aa6c6b7b3edc69f
Vladislav Prekel <misterptits@yandex.ru>
2020-12-15 19:10:11 +0700

    async project

---
+++ CSharpLabs.Lab06/CSharpLabs.Lab06.FSharp/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open System.IO
+
+let printTotalFileBytes path =
+    async {
+        let! bytes = File.ReadAllBytesAsync(path) |> Async.AwaitTask
+        let fileName = Path.GetFileName(path)
+        printfn $"File {fileName} has %d{bytes.Length} bytes"
+    }
+
+[<EntryPoint>]
+let main argv =
+    argv
+    |> Seq.map printTotalFileBytes
+    |> Async.Parallel
+    |> Async.Ignore
+    |> Async.RunSynchronously
+
+    0
